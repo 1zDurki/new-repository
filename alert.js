ca5b33dad98e77dc93cb1d8daa61bf8bf8b14519
Andrey Sliniakov <g.ali2009@yandex.ru>
2020-04-21 15:07:21 +0300

    обновлен скрипт

--- alert.js
+++ alert.js
@@ -1,21 +1,18 @@
 'use strict';
 
-let login = prompt('Who is there?', 'login');
-if (login == 'Admin') {
-	let password = prompt('Enter password' ,'pass');
-	if (password == 'General') {
-	alert ('Greeting!!!');
-	}
-	else if (password == '' || password == null) {
-	alert ('Отменено');
-	}
-	else {
-		alert('Неверный пароль');
-	}
-}
-	else if (login == '' || login == null) {
-		alert('Отменено');
-	}
-	else {
-		alert('Я тебя не знаю');
+let login = prompt('Who is there?', '')
+
+	if (login == 'Admin') {
+		let password = prompt('password?', '')
+			if (password == 'Я главный') {
+				alert('Здравствуйте')
+			} else if (password == null) {
+				alert('Отменено')
+			} else {
+				alert('Неверный пароль')
+			}
+	} else if (login == null) {
+		alert('Отмена')
+	} else {
+		alert('I dont know you')
 	}
