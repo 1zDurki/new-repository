45e9f69b0a95e573f14b2d4743b7bea3eda02b4e
Andrey Sliniakov <g.ali2009@yandex.ru>
2020-04-23 16:03:42 +0300

    новые решенные задачи по learn JS

--- alert.js
+++ alert.js
@@ -1,7 +1,82 @@
 'use strict';
 
-let value;
+/*let value;
 
 do {
 	value = prompt('input num > 100', '');
-} while (value <= 100 && value);
+} while (value <= 100 && value);*/
+
+
+
+/*let browser = prompt("input your browser's name", ""); {
+	if( browser =='Edge') {
+		alert("You've got the Edge")
+	} else if ( browser == 'Chrome' 
+		|| browser == 'Firefox' 
+		|| browser == 'Safari' 
+		|| browser == 'Opera') {
+		alert( 'Okay we support these browsers too' )
+	} else {
+		alert( 'We hope that this page looks ok!' )
+	}
+}*/
+
+
+
+/*const number = +prompt('Введите число от 1 до 3', '');
+
+switch (number) {
+	case (0):
+	alert('вы ввели число 0');
+	break;
+
+	case (1):
+	alert('вы ввели число 1');
+	break;
+
+	case (2):
+	case (3):
+	alert('вы ввели число 2 или 3');
+	break;
+}*/
+
+
+
+
+/*function checkAge(age) {
+	return (age > 18) ? thrue : confirm('Родители разрешили?');
+}
+
+
+function checkAge(age) {
+	return (age > 18) || confirm('Родители разрешили?');
+}*/
+
+
+/*function min(a, b) {
+	return a < b ? a : b;
+}*/
+
+
+
+
+/*function pow(x, n) {
+	let result = x;
+
+	for(let i = 1; i < n; i++) {
+		result *= x;
+	}
+
+	return result;
+}
+
+let x = prompt('x?', '');
+let n = prompt('n?', '');
+
+if (n < 1) {
+	alert(`Степень ${n} не поддерживается, используйте натуральное число`);
+} else {
+	alert( pow(x, n) );
+}*/
+
+
