b98d06e0419d68e479afe5872cd5834e6b2a4c17
Andrey Sliniakov <g.ali2009@yandex.ru>
2020-04-24 13:42:19 +0300

    обновлен файл с learnJS

--- alert.js
+++ alert.js
@@ -80,3 +80,13 @@ if (n < 1) {
 }*/
 
 
+/*let ask = (question, yes, no) => (confirm (question)) ? yes() : no();
+
+ask(
+	"Вы согласны?",
+	() => {alert("Вы согласились"); },
+	() => {alert("Вы отменили действие"); }
+);*/
+
+
+
