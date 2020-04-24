a52a1d17a71211d3348d295e94463cbb1daf2ef7
Andrey Sliniakov <g.ali2009@yandex.ru>
2020-04-24 14:00:52 +0300

    обновление

--- alert.js
+++ alert.js
@@ -90,3 +90,43 @@ ask(
 
 
 
+/*	name: "John",
+	surname: "Smith",
+}
+
+let name = "Pete";
+
+delete user.name;*/
+
+
+/*function isEmpty(obj) {
+	for (let key in obj) {
+		return false;
+	}
+	return thrue;
+}*/
+
+
+
+/*let salaries = {
+	John: 100,
+	Ann: 160,
+	Pete: 130,
+};
+
+let sum = 0;
+for (let key in salaries) {
+	sum += salaries[key];
+}
+
+alert(sum);*/
+
+
+
+function multiplyNumeric(obj) {
+	for(let key in obj) {
+		if (typeof obj[key] == 'number') {
+			obj[key] *= 2;
+		}
+	}
+}
